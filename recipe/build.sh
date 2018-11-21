a37b82bc21edb370965d1f772e6ca7ab198a5cfa
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-11-20 22:07:16 -0500

    Disable PIE on Linux as it breaks the executable
    Appears that symbols like `__libc_csu_fini` cannot be used for position
    independent executables (PIE). See the linked build log below for
    details. As the new Linux compiler has PIE enabled by default as part of
    the build, we need to explicitly disable this feature. Thus this commit
    adds the `-no-pie` flag. It is only added on Linux as that is the only
    compiler that has the issue.
    
    ref: https://circleci.com/gh/conda-forge/iperf-feedstock/31

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,10 @@
+# Cannot build with PIE as symbols (e.g. `__libc_csu_fini`) cannot be used.
+if [ "$(uname)" == "Linux" ] && [ "${c_compiler}" == "gcc" ]
+then
+    export CFLAGS="${CFLAGS} -fno-pie -no-pie"
+    export CXXFLAGS="${CFLAGS} -fno-pie -no-pie"
+fi
+
 ./configure --prefix="${PREFIX}" \
             --enable-pic
 make
